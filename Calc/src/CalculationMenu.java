57b34bacdef5b253f3dd8b5bed1aa36a4cd9a6c0
mydog4444 <44311299+mydog4444@users.noreply.github.com>
2019-04-18 05:57:16 -0400

    Updated
    A slightly broken Royal Flush calculator and a ton of more code. Also fixed redundancies in Calc Menu

--- Calc/src/CalculationMenu.java
+++ Calc/src/CalculationMenu.java
@@ -11,44 +11,189 @@ public class CalculationMenu extends JFrame implements ActionListener{
 		//create the JFrame
 		JFrame calcScreen = new JFrame();
 		calcScreen.setVisible(true);
-		calcScreen.setSize(750,500);
+		calcScreen.setLayout(null);
+		calcScreen.setSize(825,500);
+		calcScreen.setTitle("B.E.T. Poker Calculator");
 		
+		//ARRAYLIST DEBUGGING LABEL
 		int CNum=1;
 		//JLabel Title = new JLabel(""+Jeff.getCard(1));
-		JLabel Title = new JLabel(""+Jeff.getCard(CNum).getNum()+" "+Jeff.getCard(CNum).getSuit()+" "+Jeff.getSize());
-		Title.setBounds(500, 50, 100, 100);
+		JLabel Title = new JLabel("Royal Flush: "+Jeff.RoyalFlush()+"%");
+		Title.setBounds(500, 350, 100, 100);
 		calcScreen.add(Title);
-		JButton b=new JButton("ArrayFix");    
-		b.setBounds(100,100,150, 40);
+		JButton b=new JButton("Refresh");    
+		b.setBounds(200,400,150,40);
 		calcScreen.add(b);
+		b.setActionCommand("Refresh");
 		
-		JPanel chooseHand=new JPanel();
-		calcScreen.add(chooseHand);
-		JLabel suit=new JLabel("Choose your suit");
-		suit.setVisible(true);
-		chooseHand.add(suit);
+		String[] suits={"Spades", "Clubs", "Diamonds", "Hearts"};
+		String[] cardVals={"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+		String[] suitOpt={null,"Spades", "Clubs", "Diamonds", "Hearts"};
+		String[] cardValOpt= {null, "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 		
-		String[] suits={"Spades", "Clubs", "Diamond", "Hearts"};
-		JComboBox<String> suitOptions=new JComboBox<String>(suits);
-		suitOptions.setVisible(true);
-		chooseHand.add(suitOptions);
 		
-		JButton suitRefresh=new JButton("Refresh");
-		chooseHand.add(suitRefresh);
+		//LABEL PLACEMENT
+		JLabel suit1=new JLabel("Pick the suit of the first card in your hand:");
+		suit1.setBounds(5,10,275,15);
+		calcScreen.add(suit1);
 		
-		JLabel nums=new JLabel("Choose your number");
-		nums.setVisible(true);
-		chooseHand.add(nums);
+		JLabel nums1=new JLabel("Pick the number of the first card in your hand:");
+		nums1.setBounds(400,10,275,15);
+		calcScreen.add(nums1);
 		
-		String[] cardVals={"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
-		JComboBox<String> cardValOptions=new JComboBox<String>(cardVals);
-		cardValOptions.setVisible(true);
-		chooseHand.add(cardValOptions);
+		JLabel suit2=new JLabel("Pick the suit of the second card in your hand:");
+		suit2.setBounds(5,40,275,15);
+		calcScreen.add(suit2);
 		
-		JButton cardValRefresh=new JButton("Refresh");
-		chooseHand.add(cardValRefresh);
+		JLabel nums2=new JLabel("Pick the number of the second card in your hand:");
+		nums2.setBounds(400,40,285,15);
+		calcScreen.add(nums2);
 		
-		calcScreen.setLayout(null);
+		JLabel suit3=new JLabel("Pick the suit of the first flop card:");
+		suit3.setBounds(5,70,275,15);
+		calcScreen.add(suit3);
+		
+		JLabel nums3=new JLabel("Pick the number of the first flop card:");
+		nums3.setBounds(400,70,275,15);
+		calcScreen.add(nums3);
+		
+		JLabel suit4=new JLabel("Pick the suit of the second flop card:");
+		suit4.setBounds(5,100,275,15);
+		calcScreen.add(suit4);
+		
+		JLabel nums4=new JLabel("Pick the number of the second flop card:");
+		nums4.setBounds(400,100,275,15);
+		calcScreen.add(nums4);
+		
+		JLabel suit5=new JLabel("Pick the suit of the third flop card:");
+		suit5.setBounds(5,130,275,15);
+		calcScreen.add(suit5);
+		
+		JLabel nums5=new JLabel("Pick the number of the third flop card:");
+		nums5.setBounds(400,130,275,15);
+		calcScreen.add(nums5);
+		
+		JLabel suit6=new JLabel("(Optional) Pick the suit of the turn card:");
+		suit6.setBounds(5,160,275,15);
+		calcScreen.add(suit6);
+		
+		JLabel nums6=new JLabel("(Optional) Pick the number of the turn card:");
+		nums6.setBounds(400,160,275,15);
+		calcScreen.add(nums6);
+		
+		JLabel suit7=new JLabel("(Optional) Pick the suit of the river card:");
+		suit7.setBounds(5,190,275,15);
+		calcScreen.add(suit7);
+		
+		JLabel nums7=new JLabel("(Optional) Pick the number of the river card:");
+		nums7.setBounds(400,190,275,15);
+		calcScreen.add(nums7);
+		
+		JComboBox<String> suitOptionsUserhand1=new JComboBox<String>(suits);
+		suitOptionsUserhand1.setBounds(275,5,100,25);
+		suitOptionsUserhand1.setSelectedIndex(0);
+		//suitOptionsUserhand1.setVisible(true);
+		calcScreen.add(suitOptionsUserhand1);
+		suitOptionsUserhand1.setActionCommand("suitOptionsUserhand1");
+		//JButton cardRefresh=new JButton("Refresh");
+		//chooseHand.add(suitRefreshUserhand1);
+		
+		
+		
+		JComboBox<String> cardValOptionsUserhand1=new JComboBox<String>(cardVals);
+		cardValOptionsUserhand1.setBounds(695,5,100,25);
+		cardValOptionsUserhand1.setSelectedIndex(0);
+		//cardValOptionsUserhand1.setVisible(true);
+		calcScreen.add(cardValOptionsUserhand1);
+		cardValOptionsUserhand1.setActionCommand("cardValOptionsUserhand1");
+		
+		//JButton cardValRefreshUserhand1=new JButton("Refresh");
+		//chooseHand.add(cardValRefreshUserhand1);
+		
+		JComboBox<String> suitOptionsUserhand2=new JComboBox<String>(suits);
+		suitOptionsUserhand2.setBounds(275,35,100,25);
+		suitOptionsUserhand2.setSelectedIndex(0);
+		suitOptionsUserhand2.setVisible(true);
+		calcScreen.add(suitOptionsUserhand2);
+		suitOptionsUserhand2.setActionCommand("suitOptionsUserhand2");
+		
+		JComboBox<String> cardValOptionsUserhand2=new JComboBox<String>(cardVals);
+		cardValOptionsUserhand2.setBounds(695,35,100,25);
+		cardValOptionsUserhand2.setSelectedIndex(0);
+		cardValOptionsUserhand2.setVisible(true);
+		calcScreen.add(cardValOptionsUserhand2);
+		cardValOptionsUserhand2.setActionCommand("cardValOptionsUserhand2");
+		
+		JComboBox<String> suitOptionsflop1=new JComboBox<String>(suits);
+		suitOptionsflop1.setBounds(275,65,100,25);
+		suitOptionsflop1.setSelectedIndex(0);
+		suitOptionsflop1.setVisible(true);
+		calcScreen.add(suitOptionsflop1);
+		suitOptionsflop1.setActionCommand("suitOptionsflop1");
+		
+		JComboBox<String> cardValOptionsflop1=new JComboBox<String>(cardVals);
+		cardValOptionsflop1.setBounds(695,65,100,25);
+		cardValOptionsflop1.setSelectedIndex(0);
+		cardValOptionsflop1.setVisible(true);
+		calcScreen.add(cardValOptionsflop1);
+		cardValOptionsflop1.setActionCommand("cardValOptionsflop1");
+		
+		JComboBox<String> suitOptionsflop2=new JComboBox<String>(suits);
+		suitOptionsflop2.setBounds(275,95,100,25);
+		suitOptionsflop2.setSelectedIndex(0);
+		suitOptionsflop2.setVisible(true);
+		calcScreen.add(suitOptionsflop2);
+		suitOptionsflop2.setActionCommand("suitOptionsflop2");
+		
+		JComboBox<String> cardValOptionsflop2=new JComboBox<String>(cardVals);
+		cardValOptionsflop2.setBounds(695,95,100,25);
+		cardValOptionsflop2.setSelectedIndex(0);
+		cardValOptionsflop2.setVisible(true);
+		calcScreen.add(cardValOptionsflop2);
+		cardValOptionsflop2.setActionCommand("cardValOptionsflop2");
+		
+		JComboBox<String> suitOptionsflop3=new JComboBox<String>(suits);
+		suitOptionsflop3.setBounds(275,125,100,25);
+		suitOptionsflop3.setSelectedIndex(0);
+		suitOptionsflop3.setVisible(true);
+		calcScreen.add(suitOptionsflop3);
+		suitOptionsflop3.setActionCommand("suitOptionsflop3");
+		
+		JComboBox<String> cardValOptionsflop3=new JComboBox<String>(cardVals);
+		cardValOptionsflop3.setBounds(695,125,100,25);
+		cardValOptionsflop3.setSelectedIndex(0);
+		cardValOptionsflop3.setVisible(true);
+		calcScreen.add(cardValOptionsflop3);
+		cardValOptionsflop3.setActionCommand("cardValOptionsflop3");
+		
+		JComboBox<String> suitOptionsturn=new JComboBox<String>(suitOpt);
+		suitOptionsturn.setBounds(275,155,100,25);
+		suitOptionsturn.setSelectedIndex(0);
+		suitOptionsturn.setVisible(true);
+		calcScreen.add(suitOptionsturn);
+		suitOptionsturn.setActionCommand("suitOptionsturn");
+		
+		JComboBox<String> cardValOptionsturn=new JComboBox<String>(cardValOpt);
+		cardValOptionsturn.setBounds(695,155,100,25);
+		cardValOptionsturn.setSelectedIndex(0);
+		cardValOptionsturn.setVisible(true);
+		calcScreen.add(cardValOptionsturn);
+		cardValOptionsturn.setActionCommand("cardValOptionsturn");
+		
+		JComboBox<String> suitOptionsriver=new JComboBox<String>(suitOpt);
+		suitOptionsriver.setBounds(275,185,100,25);
+		suitOptionsriver.setSelectedIndex(0);
+		suitOptionsriver.setVisible(true);
+		calcScreen.add(suitOptionsriver);
+		suitOptionsriver.setActionCommand("suitOptionsriver");
+		
+		JComboBox<String> cardValOptionsriver=new JComboBox<String>(cardValOpt);
+		cardValOptionsriver.setBounds(695,185,100,25);
+		cardValOptionsriver.setSelectedIndex(0);
+		cardValOptionsriver.setVisible(true);
+		calcScreen.add(cardValOptionsriver);
+		cardValOptionsriver.setActionCommand("cardValOptionsriver");
+		
 		calcScreen.setVisible(true);
 		
 		
@@ -63,20 +208,102 @@ public class CalculationMenu extends JFrame implements ActionListener{
 	*/
 	//Run the menu
 	
+	b.addActionListener(this);
+	suitOptionsUserhand1.addActionListener(this);
+	suitOptionsUserhand2.addActionListener(this);
+	suitOptionsflop1.addActionListener(this);
+	suitOptionsflop2.addActionListener(this);
+	suitOptionsflop3.addActionListener(this);
+	suitOptionsturn.addActionListener(this);
+	suitOptionsriver.addActionListener(this);
+	cardValOptionsUserhand1.addActionListener(this);
+	cardValOptionsUserhand2.addActionListener(this);
+	cardValOptionsflop1.addActionListener(this);
+	cardValOptionsflop2.addActionListener(this);
+	cardValOptionsflop3.addActionListener(this);
+	cardValOptionsturn.addActionListener(this);
+	cardValOptionsriver.addActionListener(this);
 	
-	b.addActionListener(new ActionListener() {
-        
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-				Jeff.FixArray();
-				Title.setText(""+Jeff.getCard(CNum).getNum()+" "+Jeff.getCard(CNum).getSuit()+" "+Jeff.getSize());
-		}          
-      });
-	}         
+	}
 	public static void main(String[] args) {new CalculationMenu();}
+	
+	int findVal(String setCardValue, String SorN) {
+		if(SorN=="Suit") {
+			if(setCardValue==null) {
+				System.out.println("0S");
+				return 0;
+			}
+			if(setCardValue.equals("Spades")) {
+        		System.out.println("1S");
+				return 1;
+        	}
+			if(setCardValue.equals("Clubs")) {
+				System.out.println("2S");
+				return 2;	
+			}
+			if(setCardValue.equals("Diamonds")) {
+				System.out.println("3S");
+				return 3;
+			}
+			if(setCardValue.equals("Hearts")) {
+				System.out.println("4S");
+				return 4;
+			}
+		}
+		if(SorN=="Number") {
+			if(setCardValue==null) {
+				System.out.println("0N");
+				return 0;
+        	}
+        	else if(setCardValue.equals("A")) {
+        		System.out.println("1N");
+        		return 1;
+        	}
+        	else if(setCardValue.equals("Jack")) {
+        		System.out.println("11N");
+        		return 11;
+        	}
+        	else if(setCardValue.equals("Queen")) {
+        		System.out.println("12N");
+        		return 12;
+        	}
+        	else if(setCardValue.equals("King")) {
+        		System.out.println("13N");
+        		return 13;
+        	}
+        	else {
+        		System.out.println(Integer.parseInt(setCardValue)+"N");
+        		return Integer.parseInt(setCardValue);
+        	}
+		}
+		
+		return 0;
+	}
+	
+	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand().equals("Refresh")) {Jeff.RoyalFlush();}
+		else {
+		
+		JComboBox cb = (JComboBox)e.getSource();
+        String setCardValue = (String)cb.getSelectedItem();
+        
+        if(e.getActionCommand().equals("suitOptionsUserhand1")) {Jeff.setUserhand1S(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsUserhand2")) {Jeff.setUserhand2S(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsflop1")) {Jeff.setflop1S(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsflop2")) {Jeff.setflop2S(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsflop3")) {Jeff.setflop3S(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsturn")) {Jeff.setturnS(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("suitOptionsriver")) {Jeff.setriverS(findVal(setCardValue, "Suit"));}
+        if(e.getActionCommand().equals("cardValOptionsUserhand1")) {Jeff.setUserhand1N(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsUserhand2")) {Jeff.setUserhand2N(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsflop1")) {Jeff.setflop1N(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsflop2")) {Jeff.setflop2N(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsflop3")) {Jeff.setflop3N(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsturn")) {Jeff.setturnN(findVal(setCardValue, "Number"));}
+        if(e.getActionCommand().equals("cardValOptionsriver")) {Jeff.setriverN(findVal(setCardValue, "Number"));}}
+        //Jeff.FixArray();
 		
 	}
 
--- Calc/src/Calculator.java
+++ Calc/src/Calculator.java
@@ -8,8 +8,69 @@ public class Calculator {
 	Card flop1 = new Card(11, 4);
 	Card flop2 = new Card(5, 3);
 	Card flop3 = new Card(6, 3);
-	Card turn = new Card(12,4);
-	Card river;
+	Card turn = new Card(0,0);
+	Card river = new Card(0,0);
+	Card[] ActiveCards = {Userhand.getFirstCard(), Userhand.getSecondCard(), flop1, flop2, flop3, turn, river};
+	
+	//All of the cards:
+	Card AS = new Card(1,1);
+	Card IIS = new Card(2,1);
+	Card IIIS = new Card(3,1);
+	Card IVS = new Card(4,1);
+	Card VS = new Card(5,1);
+	Card VIS = new Card(6,1);
+	Card VIIS = new Card(7,1);
+	Card VIIIS = new Card(8,1);
+	Card IXS = new Card(9,1);
+	Card XS = new Card(10,1);
+	Card JS = new Card(11,1);
+	Card QS = new Card(12,1);
+	Card KS = new Card(13,1);
+	Card AC = new Card(1,2);
+	Card IIC = new Card(2,2);
+	Card IIIC = new Card(3,2);
+	Card IVC = new Card(4,2);
+	Card VC = new Card(5,2);
+	Card VIC = new Card(6,2);
+	Card VIIC = new Card(7,2);
+	Card VIIIC = new Card(8,2);
+	Card IXC = new Card(9,2);
+	Card XC = new Card(10,2);
+	Card JC = new Card(11,2);
+	Card QC = new Card(12,2);
+	Card KC = new Card(13,2);
+	Card AD = new Card(1,3);
+	Card IID = new Card(2,3);
+	Card IIID = new Card(3,3);
+	Card IVD = new Card(4,3);
+	Card VD = new Card(5,3);
+	Card VID = new Card(6,3);
+	Card VIID = new Card(7,3);
+	Card VIIID = new Card(8,3);
+	Card IXD = new Card(9,3);
+	Card XD = new Card(10,3);
+	Card JD = new Card(11,3);
+	Card QD = new Card(12,3);
+	Card KD = new Card(13,3);
+	Card AH = new Card(1,4);
+	Card IIH = new Card(2,4);
+	Card IIIH = new Card(3,4);
+	Card IVH = new Card(4,4);
+	Card VH = new Card(5,4);
+	Card VIH = new Card(6,4);
+	Card VIIH = new Card(7,4);
+	Card VIIIH = new Card(8,4);
+	Card IXH = new Card(9,4);
+	Card XH = new Card(10,4);
+	Card JH = new Card(11,4);
+	Card QH = new Card(12,4);
+	Card KH = new Card(13,4);
+	
+	ArrayList<Card> PossCard = new ArrayList<Card>();
+	
+	Card[] forRoyalFlush = {AS, AC, AD, AH, XS, XC, XD, XH, JS, JC, JD, JH, QS, QC, QD, QH, KS, KC, KD, KH};
+	
+	
 	//CONSTRUCTORS
 	public Calculator(){
 		MakeArray();
@@ -41,6 +102,7 @@ public class Calculator {
 		}*/
 	}
 	public void FixArray(){
+		MakeArray();
 		for(Card TestedCard: Cardlist) {
 			if(turn!=null) {
 				if(TestedCard.getSuit()==turn.getSuit()&&TestedCard.getNum()==turn.getNum()){
@@ -54,19 +116,214 @@ public class Calculator {
 			}
 		}
 	}
+	
 	//ACCESSORS
-	public Card getCard(int num) {
-		return Cardlist.get(num);
-	}
-	public int getSize() {
-		return Cardlist.size();
-	}
+	public Card getCard(int num) {return Cardlist.get(num);}
+	public int getSize() {return Cardlist.size();}
+	public Hand getUserhand() {return Userhand;}
+	
+	//ACCESSORS
+	public void setUserhand1N(int num){Userhand.getFirstCard().setNum(num);}
+	public void setUserhand1S(int suit){Userhand.getFirstCard().setSuit(suit);}
+	public void setUserhand2N(int num){Userhand.getSecondCard().setNum(num);}
+	public void setUserhand2S(int suit){Userhand.getSecondCard().setSuit(suit);}
+	public void setflop1N(int num){flop1.setNum(num);}
+	public void setflop1S(int suit){flop1.setSuit(suit);}
+	public void setflop2N(int num){flop2.setNum(num);}
+	public void setflop2S(int suit){flop2.setSuit(suit);}
+	public void setflop3N(int num){flop3.setNum(num);}
+	public void setflop3S(int suit){flop3.setSuit(suit);}
+	public void setturnN(int num){turn.setNum(num);}
+	public void setturnS(int suit){turn.setSuit(suit);}
+	public void setriverN(int num){river.setNum(num);}
+	public void setriverS(int suit){river.setSuit(suit);}
+	
 	/*Precondition: Given a hand
 	 */
+	
+	//puts cards in order
+	public Card[] orderCards() {
+		
+		for(int i=0; i<7; i++) {
+			for(int j=0; j<6; j++) {
+				if(ActiveCards[j].getNum()<ActiveCards[j+1].getNum()) {
+					Card TempStore = new Card(ActiveCards[j].getNum(), ActiveCards[j].getSuit());
+					ActiveCards[j].setNum(ActiveCards[j+1].getNum());
+					ActiveCards[j].setSuit(ActiveCards[j+1].getSuit());
+					ActiveCards[j+1].setNum(TempStore.getNum());
+					ActiveCards[j+1].setSuit(TempStore.getSuit());
+				}
+					
+			}
+		}
+		System.out.println(ActiveCards[0].getNum()+" "+ActiveCards[1].getNum()+" "+ActiveCards[2].getNum()+" "+ActiveCards[3].getNum()+" "+ActiveCards[4].getNum()+" "+ActiveCards[5].getNum()+" "+ActiveCards[6].getNum());
+		return ActiveCards;
+	}
+	
+	//Asks if placed cards have the parameter
+	public boolean anyPlaced(int search) {
+		if(flop1.getNum()==search || flop2.getNum()==search || flop3.getNum()==search || turn.getNum()==search || river.getNum()==search)
+			return true;
+		return false;
+	}
+	
+	public boolean anyPlaced(int search, int suit) {
+		if((flop1.getNum()==search && flop1.getSuit()==suit) || (flop2.getNum()==search && flop2.getSuit()==suit) || (flop3.getNum()==search && flop3.getSuit()==suit) || (turn.getNum()==search && turn.getSuit()==suit) || (river.getNum()==search && river.getSuit()==suit))
+				return true;
+		return false;
+	}
+	
+	public boolean anyInHand(int search) {
+		if(Userhand.getFirstCard().getNum()==search || Userhand.getSecondCard().getNum()==search)
+			return true;
+		return false;
+	}
+	
+	public boolean anyInHand(int search, int suit) {
+		if((Userhand.getFirstCard().getNum()==search && Userhand.getFirstCard().getSuit()==suit) || (Userhand.getSecondCard().getNum()==search && Userhand.getSecondCard().getSuit()==suit))
+			return true;
+		return false;
+	}
+	
+	public boolean anyActive(int search) {
+		if(anyInHand(search) || anyPlaced(search))
+			return true;
+		return false;
+	}
+	
+	public boolean anyActive(int search, int suit) {
+		if(anyInHand(search, suit) || anyPlaced(search, suit))
+			return true;
+		return false;
+	}
+	
+	public int mostPlacedSuit() {
+		int numS=0, numC=0, numD=0, numH=0;
+		for(int i=2; i<7; i++) {
+			if(ActiveCards[i].getSuit()==1)
+				numS++;
+			if(ActiveCards[i].getSuit()==2)
+				numC++;
+			if(ActiveCards[i].getSuit()==3)
+				numD++;
+			if(ActiveCards[i].getSuit()==4)
+				numH++;
+		}
+		if(numS>numC && numS>numD && numS>numH)
+			return 1;
+		if(numC>numS && numC>numD && numC>numH)
+			return 2;
+		if(numD>numS && numD>numC && numD>numH)
+			return 3;
+		if(numH>numS && numH>numC && numH>numD)
+			return 4;
+		return 0;
+	}
+	
+	
+	
+	public int mostActiveSuit() {
+		int numS=0, numC=0, numD=0, numH=0;
+		for(int i=0; i<7; i++) {
+			if(ActiveCards[i].getSuit()==1)
+				numS++;
+			if(ActiveCards[i].getSuit()==2)
+				numC++;
+			if(ActiveCards[i].getSuit()==3)
+				numD++;
+			if(ActiveCards[i].getSuit()==4)
+				numH++;
+		}
+		if(numS>numC && numS>numD && numS>numH)
+			return 1;
+		if(numC>numS && numC>numD && numC>numH)
+			return 2;
+		if(numD>numS && numD>numC && numD>numH)
+			return 3;
+		if(numH>numS && numH>numC && numH>numD)
+			return 4;
+		return 0;
+	}
+	
+	public int numSuit(int suit) {
+		int num=0;
+		for(int i=0; i<7; i++) {
+			if(ActiveCards[i].getSuit()==suit)
+				num++;
+		}
+		return num;
+	}
+
+	public ArrayList<Card> MissingCard(Card[] StartingNeeded, int suitNeeded) {
+		PossCard.clear();
+		for(int i=StartingNeeded.length-1; i>=0; i--) {
+			if(suitNeeded!=0) {
+				if(suitNeeded==StartingNeeded[i].getSuit()) {
+					PossCard.add(StartingNeeded[i]);
+				}
+			}
+			if(suitNeeded==0) {
+				PossCard.add(StartingNeeded[i]);
+			}
+		}
+		
+		for(int i=PossCard.size()-1; i>=0; i--) {
+			if(anyActive(PossCard.get(i).getNum(),PossCard.get(i).getSuit())) {
+				PossCard.remove(i);
+			}
+		}
+		
+		
+		return PossCard;
+	}
+	
+	
+	
+	//Makes a list of all of the possible cards to give the other player each rank
+	//public 
+	
+	//Tests for ranking
+	public boolean isRoyalFlush() {
+		Card[] OrderedActives = orderCards();
+		if(OrderedActives[0].getNum()==13) {
+			if(anyPlaced(13) && anyPlaced(12) && anyPlaced(11) && anyPlaced(10) && anyPlaced(1)) //This means that the royal flush was placed-it applies to both players, so it's nullified
+				return false;
+			if((anyActive(13,1) && anyActive(12,1) && anyActive(11,1) && anyActive(10,1) && anyActive(1,1)) || (anyActive(13,2) && anyActive(12,2) && anyActive(11,2) && anyActive(10,2) && anyActive(1,2)) || (anyActive(13,3) && anyActive(12,3) && anyActive(11,3) && anyActive(10,3) && anyActive(1,3)) || anyActive(13,4) && anyActive(12,4) && anyActive(11,4) && anyActive(10,4) && anyActive(1,4))
+				return true;
+			}
+		else
+			return false;
+		return false;
+	}
+	
+	
+	
+	
+	
 	public double RoyalFlush(){
+		if(numSuit(mostPlacedSuit())>=3) {
+			ArrayList<Card> RFNeeded = MissingCard(forRoyalFlush, mostPlacedSuit());
+			System.out.println(RFNeeded.get(0).getNum());
+			if(RFNeeded.size()>2) {
+				System.out.println("0.0%");
+				return 0.0;
+			}
+			else {
+				System.out.println("RF: "+RFNeeded.size()/990.0+"%");
+				return RFNeeded.size()/990.0;
+			}
+		}
+		
+		
 		return 0.0;
 	}
 	public double FourOfAKind(){
+		return 0.0;
+	}
+	public double HighCard() {
+		
+		
+		
 		return 0.0;
 	}
 	public static void main(String[] args) {
